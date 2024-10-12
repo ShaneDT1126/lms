ea239f643a86a073e71bf48f184b36968fa5a7f4
Shane <shanedelatorre2412@gmail.com>
2024-10-13 03:07:52 +0800

    Update student and teacher entity

--- lms-backend/Models/Teacher.cs
+++ lms-backend/Models/Teacher.cs
@@ -5,6 +5,11 @@
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public DateOnly Birthdate { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
 
     }
 }
