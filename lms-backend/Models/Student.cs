ad0df0eba21a0eb72bdf9bcf34632ab90d525228
Shane <shanedelatorre2412@gmail.com>
2024-10-13 01:07:08 +0800

    Add new Model

--- lms-backend/Models/Student.cs
+++ lms-backend/Models/Student.cs
@@ -2,5 +2,9 @@
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
     }
 }
