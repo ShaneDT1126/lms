f30c89d05eea41a988df14dd6ca1f64f64029128
Shane <shanedelatorre2412@gmail.com>
2024-10-13 17:50:42 +0800

    Modify forum model

--- lms-backend/Models/Forum.cs
+++ lms-backend/Models/Forum.cs
@@ -1,11 +1,13 @@
-﻿namespace lms_backend.Models
+﻿using lms_backend.Models.Base;
+
+namespace lms_backend.Models
 {
     public class Forum
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public Student Student { get; set; }
+        public User User { get; set; }
         public List<ForumComment> ForumComments { get; set; } = [];
     }
 }
