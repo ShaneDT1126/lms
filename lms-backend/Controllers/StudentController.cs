872ac6b992a62e670ed5f22c076825dc9547dfef
Shane <shanedelatorre2412@gmail.com>
2024-10-15 23:55:21 +0800

    Add Student Controller GetAllStudent

--- lms-backend/Controllers/StudentController.cs
+++ lms-backend/Controllers/StudentController.cs
@@ -1,8 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using lms_backend.ServiceInterface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace lms_backend.Controllers
 {
+    [Route("api")]
+    [ApiController]
     public class StudentController : ControllerBase
     {
+        private readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet("/students")]
+        public ActionResult GetAllStudents()
+        {
+            var students = _studentService.GetAllStudents();
+
+            if (students == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(students);
+        }
     }
 }
