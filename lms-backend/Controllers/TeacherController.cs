61232dc5e1a116800a1e9b959e3122b97557ca98
Shane <shanedelatorre2412@gmail.com>
2024-10-17 13:24:04 +0800

    Add teacher controller

--- lms-backend/Controllers/TeacherController.cs
+++ lms-backend/Controllers/TeacherController.cs
@@ -1,6 +1,35 @@
-﻿namespace lms_backend.Controllers
+﻿using lms_backend.ServiceInterface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lms_backend.Controllers
 {
-    public class TeacherController
+    [Route("api/v1")]
+    public class TeacherController : ControllerBase
     {
+        private readonly ITeacherService _service;
+
+        public TeacherController(ITeacherService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("/teachers")]
+        public ActionResult GetAllTeachers()
+        {
+            var teachers = _service.GetAllTeachers();
+
+            if (teachers == null)
+            {
+                return NotFound("No Teachers Found!");
+            }
+
+            return Ok(teachers);
+        }
+
+        [HttpGet("/teacher/{id}")]
+        public ActionResult GetTeacherById(int id)
+        {
+
+        }
     }
 }
