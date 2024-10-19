d00d03e3f81315bf1d2f1d4fed72d9406dfe5f30
Shane <shanedelatorre2412@gmail.com>
2024-10-20 00:24:08 +0800

    Add Enrollment Repository Interface

---
+++ lms-backend/RepositoryInterface/IEnrollmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using lms_backend.Models;
+
+namespace lms_backend.RepositoryInterface
+{
+    public interface IEnrollmentRepository
+    {
+        ICollection<Enrollment> GetAllEnrollmentsByCourse(int courseId);
+        ICollection<Enrollment> GetAllEnrollmentsByStudent(int studentId);
+    }
+}
--- lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
+++ lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a463feec1e3125baf5fbc207ca928ff4f9cdeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcd7b761a2bd68abf24c69efc92b66e6cec5645f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
