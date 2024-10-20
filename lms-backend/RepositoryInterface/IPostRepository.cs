5c311c845f80c96bf97a66c2b2662b3cf6d9dad7
Shane <shanedelatorre2412@gmail.com>
2024-10-20 23:49:45 +0800

    Add Post Repository Interface

---
+++ lms-backend/RepositoryInterface/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using lms_backend.Models;
+
+namespace lms_backend.RepositoryInterface
+{
+    public interface IPostRepository
+    {
+        Post GetPostById(int id);
+        ICollection<Post> GetAllPostByCourse(int courseId);
+        ICollection<Post> GetPostByTeacher(int teacherId);
+    }
+}
