eef2e7115bc819014b2687f3a564ef4a6c544429
Shane <shanedelatorre2412@gmail.com>
2024-10-23 16:38:18 +0800

    Add ForumCommentRepo

---
+++ lms-backend/Repositories/ForumCommentRepository.cs
@@ -0,0 +1,41 @@
+﻿using lms_backend.Data;
+using lms_backend.Models;
+using lms_backend.RepositoryInterface;
+
+namespace lms_backend.Repositories
+{
+    public class ForumCommentRepository : IForumCommentRepository
+    {
+        private readonly DataContext _context;
+
+        public ForumCommentRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public ICollection<ForumComment> GetAllForumComments()
+        {
+            var comments = _context.ForumComments.OrderBy(c => c.Id).ToList();
+
+            if (comments == null || !comments.Any())
+            {
+                return null;
+            }
+
+            return comments;
+        }
+
+        public ForumComment GetForumCommentById(int id)
+        {
+            var comment = _context.ForumComments.FirstOrDefault(c => c.Id == id);
+
+            if (comment == null)
+            {
+                return null;
+            }
+
+            return comment;
+        }
+    }
+}
