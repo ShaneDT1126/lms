316529b686ba5f44a59d08f4d52185e3be6e779c
Shane <shanedelatorre2412@gmail.com>
2024-10-23 16:30:01 +0800

    Add Forum Comment Dto

---
+++ lms-backend/DTOs/ForumCommentsDto.cs
@@ -0,0 +1,14 @@
+﻿using lms_backend.Models;
+
+namespace lms_backend.DTOs
+{
+    public class ForumCommentsDto
+    {
+        public int Id { get; set; }
+        public int ForumId { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public Forum Forum { get; set; }
+        public string Comment { get; set; }
+    }
+}
--- lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
+++ lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4beeab540d3d0eb178bc4c75d6daded0e17eec50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81136780734634e2a7646cfb84998a80c971a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
