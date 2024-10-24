4537d9f82eb8fc5bd721c7e86a5368b0993a30ec
Shane <shanedelatorre2412@gmail.com>
2024-10-24 15:20:49 +0800

    Add ForumComment Controller

---
+++ lms-backend/Controllers/ForumCommentController.cs
@@ -0,0 +1,42 @@
+﻿using lms_backend.ServiceInterface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lms_backend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/forumcomments")]
+    public class ForumCommentController : ControllerBase
+    {
+        private readonly IForumCommentService _forumCommentService;
+        public ForumCommentController(IForumCommentService forumCommentService)
+        {
+            _forumCommentService = forumCommentService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetForumCommentById(int id)
+        {
+            var comment = _forumCommentService.GetForumCommentById(id);
+
+            if (comment == null)
+            {
+                return NotFound($"This comment with id: {id} not found!");
+            }
+
+            return Ok(comment);
+        }
+
+        [HttpGet]
+        public ActionResult GetAllForumComments()
+        {
+            var comments = _forumCommentService.GetAllForumComments();
+
+            if (comments == null)
+            {
+                return NotFound("No Comments Found!");
+            }
+
+            return Ok(comments);
+        }
+    }
+}
--- lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
+++ lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2fe0f3f008f5d3c344327b6a6edc0520e8e4487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68dfa6fc6d10c46c8db4f5c2fcc7748e3696596c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
