70bae95849b6e33f19e0def4a9d4ec0dd4b11b22
Shane <shanedelatorre2412@gmail.com>
2024-10-24 15:47:12 +0800

    Add dependency Injection

--- lms-backend/Program.cs
+++ lms-backend/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 builder.Services.AddScoped<IForumRepository, ForumRepository>();
 builder.Services.AddScoped<ILessonRepository, LessonRepository>();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IForumCommentRepository, ForumCommentRepository>();
 
 builder.Services.AddTransient<IStudentService, StudentService>();
 builder.Services.AddTransient<ITeacherService, TeacherService>();
@@ -30,6 +31,7 @@ builder.Services.AddTransient<ICourseService, CourseService>();
 builder.Services.AddTransient<IForumService, ForumService>();
 builder.Services.AddTransient<ILessonService, LessonService>();
 builder.Services.AddTransient<IPostService, PostService>();
+builder.Services.AddTransient<IForumCommentService, ForumCommentService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
