b60f0f9360502c4798e820384a62abaede9d38a3
Shane <shanedelatorre2412@gmail.com>
2024-10-25 19:49:22 +0800

    Implement Quiz GET services

--- lms-backend/Services/QuizService.cs
+++ lms-backend/Services/QuizService.cs
@@ -1,4 +1,5 @@
-﻿using lms_backend.RepositoryInterface;
+﻿using lms_backend.DTOs;
+using lms_backend.RepositoryInterface;
 using lms_backend.ServiceInterface;
 
 namespace lms_backend.Services
@@ -13,5 +14,50 @@ namespace lms_backend.Services
         }
 
 
+        public ICollection<QuizDto>? GetAllQuiz()
+        {
+            var quizzes = _quizRepository.GetAllQuiz();
+
+            if (quizzes == null || !quizzes.Any())
+            {
+                return null;
+            }
+
+            var quizzesDto = quizzes.Select(q=> new QuizDto
+            {
+                Id = q.Id,
+                Name = q.Name,
+                CreatedAt = q.CreatedAt,
+                Description = q.Description,
+                Lesson = q.Lesson,
+                TotalScore = q.TotalScore,
+                Questions = q.Questions
+            }).ToList();
+
+            return quizzesDto;
+        }
+
+        public QuizDto GetQuizById(int id)
+        {
+            var quiz = _quizRepository.GetQuizById(id);
+
+            if (quiz == null)
+            {
+                return null;
+            }
+
+            var quizDto = new QuizDto
+            {
+                Id = quiz.Id,
+                Name = quiz.Name,
+                CreatedAt = quiz.CreatedAt,
+                Description = quiz.Description,
+                Lesson = quiz.Lesson,
+                TotalScore = quiz.TotalScore,
+                Questions = quiz.Questions
+            };
+
+            return quizDto;
+        }
     }
 }
