8e3b42287467c53bbb925cf9d21c59732d88130e
Shane <shanedelatorre2412@gmail.com>
2024-10-25 19:30:55 +0800

    Add Quiz Dto

---
+++ lms-backend/DTOs/QuizDto.cs
@@ -0,0 +1,16 @@
+﻿using lms_backend.Models;
+
+namespace lms_backend.DTOs
+{
+    public class QuizDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public Lesson Lesson { get; set; }
+        public int TotalScore { get; set; }
+
+        public List<Question> Questions { get; set; }
+    }
+}
--- lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
+++ lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4537d9f82eb8fc5bd721c7e86a5368b0993a30ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70bae95849b6e33f19e0def4a9d4ec0dd4b11b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
