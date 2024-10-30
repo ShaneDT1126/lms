8d47b77c0f91bd7cd06c1660b5c8eb365caffeed
Shane <shanedelatorre2412@gmail.com>
2024-10-30 22:49:21 +0800

    Update lesson repo implement GetAllLessons and GetAllLessonProgressionsByLesson

--- lms-backend/Repositories/LessonRepository.cs
+++ lms-backend/Repositories/LessonRepository.cs
@@ -26,5 +26,35 @@ namespace lms_backend.Repositories
             return lesson;
         }
 
+        public ICollection<Lesson>? GetAllLessons()
+        {
+            var lessons = _dataContext.Lessons.OrderBy(l => l.Id).ToList();
+
+            if (lessons == null || !lessons.Any())
+            {
+                return null;
+            }
+
+            return lessons;
+        }
+
+        public ICollection<LessonProgression>? GetAllLessonProgressionsByLesson(int lessonId)
+        {
+            var lesson = _dataContext.Lessons.FirstOrDefault(l => l.Id == lessonId);
+
+            if (lesson == null)
+            {
+                return null;
+            }
+
+            var lessonProgressions = lesson.LessonProgressions.ToList();
+
+            if (lessonProgressions == null || !lessonProgressions.Any())
+            {
+                return null;
+            }
+
+            return lessonProgressions;
+        }
     }
 }
