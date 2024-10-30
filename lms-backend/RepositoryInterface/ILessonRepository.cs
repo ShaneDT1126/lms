f7f80022aa012a91405c42e4901c0354891fad57
Shane <shanedelatorre2412@gmail.com>
2024-10-30 22:44:37 +0800

    Update Lesson Repo, add 2 new GET services GetAllLessons and GetAllLessonProgressionsByLesson

--- lms-backend/RepositoryInterface/ILessonRepository.cs
+++ lms-backend/RepositoryInterface/ILessonRepository.cs
@@ -5,5 +5,7 @@ namespace lms_backend.RepositoryInterface
     public interface ILessonRepository
     {
         Lesson GetLessonById(int id);
+        ICollection<Lesson>? GetAllLessons();
+        ICollection<LessonProgression>? GetAllLessonProgressionsByLesson(int lessonId);
     }
 }
