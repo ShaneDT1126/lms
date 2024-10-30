c1287e89353370ba00a85eabd8833e0085c0fa9d
Shane <shanedelatorre2412@gmail.com>
2024-10-30 23:20:04 +0800

    Update Lesson service implemented the missing services

--- lms-backend/Services/LessonService.cs
+++ lms-backend/Services/LessonService.cs
@@ -14,7 +14,7 @@ namespace lms_backend.Services
             _repository = repository;
         }
 
-        public Lesson GetLessonById(int id)
+        public LessonDto GetLessonById(int id)
         {
             var lesson = _repository.GetLessonById(id);
 
@@ -23,7 +23,58 @@ namespace lms_backend.Services
                 return null;
             }
 
-            return lesson;
+            var lessonDto = new LessonDto
+            {
+                Id = lesson.Id,
+                Name = lesson.Name,
+                CreatedAt = lesson.CreatedAt,
+                Description = lesson.Description,
+                Content = lesson.Content
+            };
+
+            return lessonDto;
+        }
+
+        public ICollection<LessonDto> GetAllLessons()
+        {
+            var lessons = _repository.GetAllLessons();
+
+            if (lessons == null)
+            {
+                return null;
+            }
+
+            var lessonsDto = lessons.Select(l => new LessonDto
+            {
+                Id = l.Id,
+                Name = l.Name,
+                CreatedAt = l.CreatedAt,
+                Description = l.Description,
+                Content = l.Content
+            }).ToList();
+
+            return lessonsDto;
+        }
+
+        public ICollection<LessonProgressionDto> GetAllLessonProgressionsByLesson(int lessonId)
+        {
+            var lessonProgressions = _repository.GetAllLessonProgressionsByLesson(lessonId);
+
+            if (lessonProgressions == null)
+            {
+                return null;
+            }
+
+            var lessonProgressionDto = lessonProgressions.Select(lp => new LessonProgressionDto
+            {
+                LessonId = lp.LessonId,
+                StudentId = lp.StudentId,
+                Lesson = lp.Lesson,
+                Student = lp.Student,
+                Status = lp.Status
+            }).ToList();
+
+            return lessonProgressionDto;
         }
     }
 }
