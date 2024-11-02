9ec757b704afb0a606103859a31a32319a669c20
Shane <shanedelatorre2412@gmail.com>
2024-11-02 23:23:34 +0800

    Add question dto fields

--- lms-backend/DTOs/QuestionDto.cs
+++ lms-backend/DTOs/QuestionDto.cs
@@ -2,5 +2,10 @@
 {
     public class QuestionDto
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Choices { get; set; } = [];
+        public string StudentAnswer { get; set; } = string.Empty;
+        public string CorrectAnswer { get; set; }
     }
 }
--- lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
+++ lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e66a19943ed2b3bcfa1b9c7978b7c6f5bc56dfdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cbba3ca9a45bd60cc143b2425871628918dcf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
