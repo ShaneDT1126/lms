6eb797edf0bf97c800fdca7b38935bd8130ac4ee
Shane <shanedelatorre2412@gmail.com>
2024-11-04 23:52:29 +0800

    Implement the get method in IQuestionRepo

---
+++ lms-backend/Repositories/QuestionRepository.cs
@@ -0,0 +1,29 @@
+﻿using lms_backend.Data;
+using lms_backend.Models;
+using lms_backend.RepositoryInterface;
+
+namespace lms_backend.Repositories
+{
+    public class QuestionRepository : IQuestionRepository
+    {
+        private readonly DataContext _context;
+
+        public QuestionRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public ICollection<Question> GetAllQuestions()
+        {
+            var questions = _context.Question.OrderBy(q => q.Id).ToList();
+
+            if (questions.Count > 0 || questions == null)
+            {
+                return null;
+            }
+
+            return questions;
+        }
+    }
+}
--- lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
+++ lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec757b704afb0a606103859a31a32319a669c20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4258c858e4d8a244fe8292cf7e01e7c806c8a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
