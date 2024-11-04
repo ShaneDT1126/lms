3e15656b1c3a28315eea356e5f5ee1e38abcd22e
Shane <shanedelatorre2412@gmail.com>
2024-11-05 00:08:46 +0800

    Add Question Controller, add new endpoint getallquestions

---
+++ lms-backend/Controllers/QuestionController.cs
@@ -0,0 +1,30 @@
+﻿using lms_backend.ServiceInterface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lms_backend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/questions")]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IQuestionService _questionService;
+
+        public QuestionController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllQuestions()
+        {
+            var questions = _questionService.GetAllQuestions();
+
+            if (questions == null)
+            {
+                return NotFound("No questions found!");
+            }
+
+            return Ok(questions);
+        }
+    }
+}
