48369482a3ce3a161463fb2a176080e94fa3a3b6
Shane <shanedelatorre2412@gmail.com>
2024-11-05 00:05:30 +0800

    Add QuestionService implemented the service getallquestions

---
+++ lms-backend/Services/QuestionService.cs
@@ -0,0 +1,37 @@
+﻿using lms_backend.DTOs;
+using lms_backend.RepositoryInterface;
+using lms_backend.ServiceInterface;
+
+namespace lms_backend.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private readonly IQuestionRepository _questionRepository;
+
+        public QuestionService(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        public ICollection<QuestionDto> GetAllQuestions()
+        {
+            var questions = _questionRepository.GetAllQuestions();
+
+            if (questions == null)
+            {
+                return null;
+            }
+
+            var questionDto = questions.Select(q => new QuestionDto
+            {
+                Id = q.Id,
+                Title = q.Title,
+                Choices = q.Choices,
+                StudentAnswer = q.StudentAnswer,
+                CorrectAnswer = q.CorrectAnswer
+            }).ToList();
+
+            return questionDto;
+        }
+    }
+}
--- lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
+++ lms-backend/obj/Debug/net8.0/lms-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb797edf0bf97c800fdca7b38935bd8130ac4ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd96d751dbc69b23bf7526efe8007d1639fa2707")]
 [assembly: System.Reflection.AssemblyProductAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lms-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
